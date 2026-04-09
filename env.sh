40a57762c68881ce7cedfbdd31b1feb6644dd38a
zzq <you@example.com>
2026-04-09 23:42:53 +0800

    fix: make env.sh repo-relative

--- env.sh
+++ env.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-export LA32R_TOOLCHAIN_PREFIX=/home/zzq/code/Nagi/la32r-toolchains/install/bin/loongarch32r-linux-gnusf-
+_la32r_env_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export LA32R_TOOLCHAIN_PREFIX="${_la32r_env_dir}/install/bin/loongarch32r-linux-gnusf-"
+unset _la32r_env_dir
